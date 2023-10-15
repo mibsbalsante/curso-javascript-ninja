3693bef76198b37987e50c90b5932760ab919326
Melissa <mibsmyrtr@gmail.com>
2023-10-15 00:53:08 -0300

    Correção

--- challenge-28/challenge-28.js
+++ challenge-28/challenge-28.js
@@ -26,34 +26,147 @@
   - Utilize a lib DOM criada anteriormente para facilitar a manipulação e
   adicionar as informações em tela.
   */
-	var $cep = document.querySelector('[data-js="cep"]');
-	var $fields = document.querySelectorAll('[data-js="input"]');
-	var $form = document.querySelector('[data-js="form"]');
-	var $message = document.querySelector('[data-js="message"]');
+	function DOM (selector) {
+		this.$elements = document.querySelectorAll(selector);
+	}
 	
-	function handleAddress(address) {
-		Array.prototype.map.call($fields, function($field) {
-			$field.value = address[$field.getAttribute('name')] || '';
+	DOM.prototype.get = function get() {
+		return this.$elements;
+	}
+
+	DOM.prototype.on = function on(eventType, callback) {
+		Array.prototype.forEach.call(this.$elements, function($element) {
+			$element.addEventListener(eventType, callback, false);
+		})
+	}
+
+	DOM.prototype.off = function off(eventType, callback) {
+		Array.prototype.forEach.call(this.$elements, function($element) {
+			$element.removeEventListener(eventType, callback, false);
 		})
 	}
 	
-	$form.addEventListener('submit', function(event) {
+	DOM.prototype.call = function call(name, callback) {
+		return Array.prototype[name].call(this.$elements, callback);
+	}
+
+  DOM.prototype.forEach = function forEach(callback) {
+		this.call('forEach', callback);
+	}
+
+  DOM.prototype.map = function map(callback) {
+		this.call('map', callback);
+	}
+
+  DOM.prototype.filter = function filter(callback) {
+		this.call('filter', callback);
+	}
+
+  DOM.prototype.reduce = function reduce(callback) {
+		this.call('reduce', callback);
+	}
+
+  DOM.prototype.reduceRight = function reduceRight(callback) {
+		this.call('reduceRight', callback);
+	}
+
+  DOM.prototype.every = function every(callback) {
+		this.call('every', callback);
+	}
+
+  DOM.prototype.some = function some(callback) {
+		this.call('some', callback);
+	}
+	
+	DOM.getType = function(element) {
+		return Object.prototype.toString.apply(element)
+			.replace(/^(\[object\s)|(\])$/g, '');
+	}
+
+	DOM.isArray = function(element) {
+		return this.getType(element) === 'Array';
+	}
+	DOM.isObject = function(element) {
+		return this.getType(element) === 'Object';
+	}
+	DOM.isFunction = function(element) {
+		return this.getType(element) === 'Function';
+	}
+	DOM.isNumber = function(element) {
+		return this.getType(element) === 'Number';
+	}
+	DOM.isString = function(element) {
+		return this.getType(element) === 'String';
+	}
+	DOM.isBoolean = function(element) {
+		return this.getType(element) === 'Boolean';
+	}
+	DOM.isNull = function(element) {
+		const type = this.getType(element);
+		return type === 'Null' || type === 'Undefined';
+	}
+	
+	var request = new XMLHttpRequest();
+	
+	var $cep = new DOM('[data-js="cep"]');
+	var $form = new DOM('[data-js="form"]');
+	
+	var cep;
+	
+	function handleFormatData(data) {
+		var address;
+		
+		try {
+			address = JSON.parse(data);	
+		} catch (e) {
+			address = null;
+		}
+		
+		return address;
+	}
+	
+	function handleAddress(data) {
+		var address = handleFormatData(data);
+		
+		if (!address) {
+			handleMessage('error');
+			return;
+		}
+		
+		var $fields = new DOM('[data-js="field"]');
+		$fields.forEach(function($field) {
+			$field.textContent = address[$field.dataset.name] || '';
+		})
+		
+		handleMessage('success');
+	}
+	
+	function handleMessage(type) {
+		var message = {
+			loading: `Buscando informações para o CEP ${cep}...`,
+			error: `Não encontramos o endereço para o CEP ${cep}.`,
+			success: `Endereço referente ao CEP ${cep}:`,
+		};
+		var $message = new DOM('[data-js="message"]');
+		$message.get()[0].textContent = message[type];
+	}
+	
+	function handleSubmitForm(event) {
 		event.preventDefault();
-		
-		var cep = $cep.value;
-		var request = new XMLHttpRequest();
-		
-		$message.textContent = `Buscando informações para o CEP ${cep}...`;
+		cep = $cep.get()[0].value;
+		handleMessage('loading');
 		
 		request.onload = function() {
-			handleAddress(JSON.parse(this.responseText));
-			$message.textContent = `Endereço referente ao CEP ${cep}.`;
+			handleAddress(this.responseText);
 		}
+	
 		request.onerror = function() {
-			var cep = $cep.value;
-			$message.textContent = `Não encontramos o endereço para o CEP ${cep}.`;
+			handleMessage('error');
 		}
+	
 		request.open('get', `https://viacep.com.br/ws/${cep}/json/`);
 		request.send();
-	});
+	}
+	
+	$form.on('submit', handleSubmitForm);
 })();
